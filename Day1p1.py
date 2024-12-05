5d535881c75ff0065dc2217f5e658442e4601178
BenTheBomb <65471744+BenTheBomb@users.noreply.github.com>
2024-12-05 17:07:07 -0500

    Add files via upload

---
+++ Day1p1.py
@@ -0,0 +1,18 @@
+array1 = []
+array2 = []
+
+with open("Day1.txt", "r") as f:
+    for line in f:
+        strArray = line.split()
+        array1.append(int(strArray[0]))
+        array2.append(int(strArray[1]))
+
+array1.sort()
+array2.sort()
+
+count = 0
+for i in range(len(array1)):
+    count += abs(array1[i] - array2[i])
+    print(i)
+    print(count)
+
